1f18e7effeffb0bd8ae199700f1d31b14ebbca66
hsiao-i <eva090029@gmail.com>
2025-06-25 00:08:55 +0800

    feat: Add gsap to project 1. Import gsap package 2. Create basic horizontal scroll trigger

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  </header>
-
   <RouterView />
 </template>
 
---
+++ src/views/HomePage.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const horizontal = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+  const contents = gsap.utils.toArray('.content');
+  console.log(contents);
+
+  gsap.to(contents, {
+    xPercent: -100 * (contents.length - 1),
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '.horizontal',
+      pin: true,
+      scrub: 0.1,
+      end: '+=3000',
+      // end: () => '+=' + (contents.length - 1) * window.innerWidth,
+    },
+  });
+});
+</script>
+
+<template>
+  <main>
+    <section ref="horizontal" id="horizontal" class="horizontal flex flex-nowrap">
+      <div class="content h-screen w-screen flex-shrink-0 bg-pink-500">123</div>
+      <div class="content h-screen w-screen flex-shrink-0 bg-yellow-500">123</div>
+    </section>
+  </main>
+</template>
